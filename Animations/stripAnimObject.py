d0440c25b0b16aaa3a69ba14389d695496011cdd
vaclavjanosek <vasekjanosek@gmail.com>
2018-11-16 19:33:38 +0100

    refactoring + comments

--- Animations/stripAnimObject.py
+++ Animations/stripAnimObject.py
@@ -1,29 +1,43 @@
 import pygame
 
+# default background color
+BACKGROUND_COLOR = (86, 118, 255)
+# size of one sprite in pixels
+PIXEL_SIZE = 16
 
-# represent one animation object
-class StripAnimObject(object):
+
+# represents one animation object
+# x - x coordinates in image
+# y - y coordinates in image
+# count_to_right - number of sprites creating animation object
+class AnimationObject(object):
+
+    # initialization of AnimationObject
     def __init__(self, filename, x, y, count_to_right):
-        self.filename = filename
+
         self.count_to_right = count_to_right
         self.x = x
         self.y = y
         self.images = []
         self.img = pygame.image.load(filename)
-        self.actualPicture = 0
+        self.actualSpriteIndex = 0
 
+        # loading sprites from given image
         for i in range(0, count_to_right):
-            rect = pygame.Rect((x + (i * 16), y, 16, 16))
+            rect = pygame.Rect((x + (i * PIXEL_SIZE), y, PIXEL_SIZE, PIXEL_SIZE))
             img = pygame.Surface(rect.size).convert()
-            img.set_colorkey((86, 118, 255))
+            img.set_colorkey(BACKGROUND_COLOR)
             img.blit(self.img, (0, 0), rect)
             self.images.append(img)
 
+    # sets actualSpriteIndex to next sprite in row
     def next(self, slowdown=0):
-        if slowdown == 0:
-            self.actualPicture += 1
-        if self.actualPicture == self.count_to_right:
-            self.actualPicture = 0
-            return self.actualPicture
 
-        return self.actualPicture
+        if slowdown == 0:
+            self.actualSpriteIndex += 1
+
+        if self.actualSpriteIndex == self.count_to_right:
+            self.actualSpriteIndex = 0
+            return self.actualSpriteIndex
+
+        return self.actualSpriteIndex
